81c45c3d2d624a8ebf2f56c044ae91454e668251
mmajewski <mmajewski@infusion.com>
2014-10-01 13:41:37 +0200

    fxi

--- Gruntfile.js
+++ Gruntfile.js
@@ -323,7 +323,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
-            'views/{,*/}*.html',
+            'scripts/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*'
           ]
